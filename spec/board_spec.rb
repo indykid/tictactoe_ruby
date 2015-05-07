be1a4285ed30361c7516df78db434e008e89a7d5
jarkyn <jarkyn@ymail.com>
2015-05-07 12:18:55 +0100

    refactors tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,12 +1,13 @@
 require 'board'
 describe Board do
+  let(:board) { Board.new }
+
   it 'is empty at the start' do
-    board = Board.new
     expect(board.cells).to eq([])
   end
 
   it 'has a default size' do
-    board = Board.new
     expect(board.size).to eq(3)
   end
+
 end

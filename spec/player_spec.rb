88108432fa01eb572962aef990c04456b2189f00
jarkyn <jarkyn@ymail.com>
2015-05-13 01:00:26 +0100

    extends Ui class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -20,22 +20,43 @@ class FakeUi
 end
 
 class Ui
-  attr_reader :output
+  attr_reader :output, :input
 
-  def initialize(output)
+  def initialize(output, input)
     @output = output
+    @input  = input
   end
 
   def show(message)
     @output.puts(message)
   end
+
+  def ask_for_position
+    show('enter next move')
+  end
+
+  def capture_response
+    @input.gets.chomp
+  end
 end
 
 describe Ui do
   it 'displays messages' do
-    ui = Ui.new(StringIO.new)
+    ui = Ui.new(StringIO.new, StringIO.new)
     ui.show('hello')
     expect(ui.output.string).to eq("hello\n")
+  end
+
+  it 'asks user for the position' do
+    ui = Ui.new(StringIO.new, StringIO.new)
+    ui.ask_for_position
+    expect(ui.output.string).to eq("enter next move\n")
+  end
+
+  it 'receives input from user' do
+    ui = Ui.new(StringIO.new, StringIO.new('0'))
+    ui.capture_response
+    expect(ui.input.string).to eq('0')
   end
 end
 

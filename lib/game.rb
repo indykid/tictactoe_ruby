7387bbdfda987e30e07de4c24acdd1cd1e4e6b6b
jarkyn <jarkyn@ymail.com>
2015-05-17 08:56:30 +0100

    replaces @ui with reader, renames @players into @queue

--- lib/game.rb
+++ lib/game.rb
@@ -4,7 +4,7 @@ class Game
     @player_x = player_x
     @player_o = player_o
     @ui = ui
-    @players = players
+    @queue = queue
   end
 
   def over?
@@ -19,25 +19,27 @@ class Game
     !winner? && board.full?
   end
 
-  def play_turn
-    @ui.visualise(board)
-    current_player = player_in_turn
-    position = current_player.pick_move(@ui)
-    if valid_move?(position)
-      board.add_move(position, current_player.mark)
-    else
-      @ui.show(Ui::ALERT)
-    end
-  end
-
   def play
     while !over?
       play_turn
     end
-    @ui.visualise(board)
+    ui.visualise(board)
+  end
+
+  def play_turn
+    ui.visualise(board)
+    current_player = player_in_turn
+    position = current_player.pick_move(ui)
+    if valid_move?(position)
+      board.add_move(position, current_player.mark)
+    else
+      ui.show(Ui::ALERT)
+    end
   end
 
   private
+
+  attr_reader :board, :ui
 
   def valid_move?(position)
     board.available.include?(position)
@@ -45,14 +47,10 @@ class Game
 
   def player_in_turn
     #board.moves.length.even? ? @player_x : @player_o
-    @players.next
+    @queue.next
   end 
 
-  private
-
-  attr_reader :board
-
-  def players
+  def queue
     [@player_x, @player_o].cycle
   end
 end

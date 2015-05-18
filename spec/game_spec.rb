c2a2c18e913801645e6378bf72e2a759b16c6484
jarkyn <jarkyn@ymail.com>
2015-05-18 18:28:00 +0100

    BROKEN: removes fakes to be rewritten.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,7 +6,8 @@ describe Game do
   let(:board) { Board.new }
   let(:player_x) { Player.new(:x) }
   let(:player_o) { Player.new(:o) }
-  let(:output) { StringIO.new }
+  #let(:display) { instance_double("UI") }
+  #let(:display) { FakeDisplay.new }
 
   it 'is over when there is a winner' do
     game = setup_game
@@ -33,6 +34,7 @@ describe Game do
     game = setup_game([0])
     game.play_turn
 
+    # UI test, misplaced
     expect(output.string).to start_with("0 1 2\n3 4 5\n6 7 8\n")
   end
 
@@ -86,48 +88,5 @@ describe Game do
   def setup_game(inputs = [])
     ui = FakeUi.new(inputs, output)
     Game.new(board, player_x, player_o, ui)
-  end
-
-end
-
-class FakeUi
-  def initialize(inputs, output)
-    @input = setup_input(inputs)
-    @output = output
-  end
-
-  def setup_input(inputs)
-    StringIO.new(inputs.join("\n"))
-  end
-
-  def visualise(board)
-    show(output_friendly(board)) 
-  end
-
-  def show(message)
-    @output.puts(message)
-  end
-
-  def output_friendly(board)
-    split_into_lines(board).map do |line|
-      line.join(' ')
-    end
-  end
-
-  def split_into_lines(board)
-    board.state.each_slice(board.size)
-  end
-
-  def ask_for_position
-    @output.puts(Ui::PROMPT)
-  end
-
-  def take_input
-    @input.gets.chomp
-  end
-
-  def capture_position
-    ask_for_position
-    take_input.to_i
   end
 end

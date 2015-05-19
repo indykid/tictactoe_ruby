a295fc289ce9fab7b20b92d7d613e06542c9de78
jarkyn <jarkyn@ymail.com>
2015-05-19 12:37:43 +0100

    refactors FakeUi in Game tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -30,14 +30,6 @@ describe Game do
     expect(board.taken?(0)).to be(true)
   end
 
-  it 'gets Ui to visualise board' do
-    game = setup_game([0])
-    game.play_turn
-
-    # UI test, misplaced
-    expect(output.string).to start_with("0 1 2\n3 4 5\n6 7 8\n")
-  end
-
   it 'only plays into free cells' do
     game = setup_game([0, 0])
     2.times { game.play_turn }
@@ -48,13 +40,6 @@ describe Game do
   it 'plays only existing cells' do
     game = setup_game(['10'])
     expect { game.play_turn }.to_not change { board.moves }
-  end
-
-  it 'gets Ui to display invalid move message' do
-    game = setup_game([0, 0])
-    2.times { game.play_turn }
-
-    expect(output.string).to include(Ui::PROMPT)
   end
 
   it 'tracks player turns' do
@@ -79,14 +64,24 @@ describe Game do
     expect(game.over?).to be(true)
   end
 
-  it 'gets Ui to visualize the board one last time after it is over' do
-    game = setup_game([0, 4, 2, 1, 5, 8, 6, 3, 7])
-    game.play
-    expect(output.string).to end_with("x o x\no o x\nx x o\n")
-  end
-
   def setup_game(inputs = [])
     ui = FakeUi.new(inputs, output)
     Game.new(board, player_x, player_o, ui)
+  end
+end
+
+class FakeUi
+  def initialize(inputs, output)
+    @inputs = inputs
+  end
+
+  def visualise(board)
+  end
+
+  def capture_position
+    @inputs.shift
+  end
+
+  def show(message)
   end
 end

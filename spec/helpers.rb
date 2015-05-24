148a0609b836f8bf6bafb1d68718510cd36425c8
jarkyn <jarkyn@ymail.com>
2015-05-24 21:23:15 +0100

    detes all code

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -1,10 +1,1 @@
-def add_multiple_moves(mark, positions, board)
-  positions.each do |position|
-    board.add_move(position, mark)
-  end
-end
 
-def fill_the_board(board)
-  add_multiple_moves(:x, [0, 2, 5, 6, 7], board)
-  add_multiple_moves(:o, [1, 3, 4, 8], board)
-end

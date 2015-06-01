c24e5ada23384e9c6a9af31cd9bce11fcba9dc52
jarkyn <jarkyn@ymail.com>
2015-06-01 11:28:39 +0100

    corrects Ui tests and Ui double after refactorings

--- spec/support/doubles/fake_ui.rb
+++ spec/support/doubles/fake_ui.rb
@@ -1,3 +1,5 @@
+require 'invalid_move_error'
+
 class FakeUi
   def initialize(input, output)
     @input = input
@@ -5,12 +7,24 @@ class FakeUi
   end
 
   def get_move_from_user
-    input.gets.to_i
+    move = input.gets.chomp
+    raise InvalidMoveError if move.to_i.to_s != move
+    move.to_i
   end
 
   def alert
   end
 
+  def display(board_state)
+    show(user_friendly(board_state))
+  end
+
   private
   attr_reader :input
+
+  def show(message)
+  end
+
+  def user_friendly(board_state)
+  end
 end
--- spec/ui_spec.rb
+++ spec/ui_spec.rb
@@ -38,15 +38,21 @@ describe Ui do
   end
 
   it 'errors if non-numeric input' do
-    input = StringIO.new("a")
+    input = StringIO.new("a\n")
     ui = Ui.new(input, output)
 
     expect { ui.get_move_from_user }.to raise_error(InvalidMoveError)
   end
 
-  it 'shows the board' do
-    board = Board.new
-    ui.display(board.state_by_rows)
+  it 'does not error if numeric input' do
+    input = StringIO.new("0\n")
+    ui = Ui.new(input, output)
+
+    expect{ui.get_move_from_user}.not_to raise_error
+  end
+
+  it 'shows user friendly board' do
+    ui.display([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
 
     expect(output.string).to eq(
       " 0 | 1 | 2 \n-----------\n 3 | 4 | 5 \n-----------\n 6 | 7 | 8 \n"

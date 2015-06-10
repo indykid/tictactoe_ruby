c7d20882accbc87a0cefad7ae2e0c705d7a51319
jarkyn <jarkyn@ymail.com>
2015-06-10 17:10:39 +0100

    improves FakePlayer, changes spec setup

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,44 +1,43 @@
 require 'spec_helper'
 require 'board'
 require 'game'
-require 'player'
 
 describe Game do
 
-  let(:board) { Board.new }
-  let(:ui) { instance_double(Ui).as_null_object }
-  let(:game) { 
-    player_x = Player.new(:x, ui) 
-    player_o = Player.new(:o, ui)
-    Game.new(board, ui, player_x, player_o)
- }
+  let(:ui)       { instance_double(Ui).as_null_object }
+  let(:board)    { Board.new }
+  let(:player_x) { FakePlayer.new(:x) }
+  let(:player_o) { FakePlayer.new(:o) }
 
   it 'is not over at the start' do
+    game = setup_game_with_board
     expect(game.over?).to be(false)
   end
 
   it 'is over if won' do
-    make_win
+    board = Board.new([:x, :x, :x, :o, :o, nil, nil, nil, nil])
+    game = setup_game_with_board(board)
 
     expect(game.over?).to be(true)
   end
 
   it 'is over when drawn' do
-    make_draw
+    board = Board.new([:x, :x, :o, :o, :x, :o, :x, :o, :x])
+    game = setup_game_with_board(board)
 
     expect(game.over?).to be(true)
   end
 
   it 'adds player moves to the board' do
-    setup_ui([0])
+    game = setup_game_with_players(FakePlayer.new(:x, [0]))
 
     game.play_turn
 
     expect(board.player_at(0)).to eq(:x)
   end
 
-  it 'tracks player turns' do
-    setup_ui([0, 1])
+  it 'switches player turns' do
+    game = setup_game_with_players(*setup_players([0], [1]))
 
     2.times { game.play_turn }
 
@@ -46,7 +45,7 @@ describe Game do
   end
 
   it 'gets Ui to display the board' do
-    setup_ui([0])
+    game = setup_game_with_players(FakePlayer.new(:x, [0]))
 
     game.play_turn
 
@@ -54,7 +53,7 @@ describe Game do
   end
 
   it 'gets Ui to alert on invalid move' do
-    setup_ui(['a', 1])
+    game = setup_game_with_players(FakePlayer.new(:x, ['a', 0]) )
 
     game.play_turn
 
@@ -62,7 +61,7 @@ describe Game do
   end
 
   it 'gets Ui to greet' do
-    setup_ui([0, 3, 1, 4, 2])
+    game = setup_for_win
 
     game.play
 
@@ -70,7 +69,7 @@ describe Game do
   end
 
   it 'plays till win' do
-    setup_ui([0, 3, 1, 4, 2])
+    game = setup_for_win
 
     game.play
 
@@ -78,7 +77,7 @@ describe Game do
   end
 
   it 'plays till draw' do
-    setup_ui([0, 3, 1, 4, 2])
+    game = setup_for_draw
 
     game.play
 
@@ -86,7 +85,7 @@ describe Game do
   end
 
   it 'gets ui to display board at the end' do
-    setup_ui([0, 3, 1, 4, 2])
+    game = setup_for_win
 
     game.play
 
@@ -94,7 +93,7 @@ describe Game do
   end
 
   it 'gets Ui to display game over at the end' do
-    setup_ui([0, 4, 2, 1, 7, 5, 3, 6, 8])
+    game = setup_for_draw
 
     game.play
 
@@ -102,7 +101,7 @@ describe Game do
   end
 
   it 'gets Ui to display winner at the end of the won game' do
-    setup_ui([0, 3, 1, 4, 2])
+    game = setup_for_win
 
     game.play
 
@@ -110,14 +109,44 @@ describe Game do
   end
 
   it 'gets Ui to announce draw when drawn' do
-    setup_ui([0, 4, 2, 1, 7, 5, 3, 6, 8])
+    game = setup_for_draw
 
     game.play
 
     expect(ui).to have_received(:display_draw)
   end
 
-  def setup_ui(inputs = [])
-    allow(ui).to receive(:get_move_from_user).and_return(*inputs)
+  def setup_for_win
+    player_x, player_o = setup_players([0, 1, 2], [3, 4])
+    setup_game_with_players(player_x, player_o)
+  end
+
+  def setup_for_draw
+    player_x, player_o = setup_players([0, 1, 4, 5, 6], [2, 3, 7, 8])
+    setup_game_with_players(player_x, player_o)
+  end
+
+  def setup_game_with_players(player_x = player_x, player_o = player_o)
+    Game.new(board, ui, player_x, player_o)
+  end
+
+  def setup_game_with_board(board = board)
+    Game.new(board, ui, player_x, player_o)
+  end
+
+  def setup_players(x_moves, o_moves = [])
+    [FakePlayer.new(:x, x_moves), FakePlayer.new(:o, o_moves)]
+  end
+
+  class FakePlayer
+    attr_reader :moves, :mark
+    def initialize(mark, moves = [])
+      @mark = mark
+      @moves = moves
+    end
+
+    def pick_position
+      moves.shift
+    end
   end
 end

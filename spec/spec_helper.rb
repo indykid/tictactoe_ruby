6a645498afbdb8165a09c641930fbc2d1a79f454
jarkyn <jarkyn@ymail.com>
2015-06-11 18:04:56 +0100

    cleans up after test setup refactor

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 #require 'simplecov'
 #SimpleCov.start
 
-require 'support/helpers/helpers.rb'
-
 RSpec.configure do |config|
-  config.include BoardSetUpHelpers
+  #config.include BoardSetUpHelpers
 end
